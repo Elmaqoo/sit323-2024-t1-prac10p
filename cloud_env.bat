63ab30d2ca2a869f667eb70ee3d74995fcbdb463
Elmaqoo <echan8589@gmail.com>
2024-05-18 16:42:36 +1000

    final

---
+++ cloud_env.bat
@@ -0,0 +1,7 @@
+ECHO OFF
+CLS
+SET PATH=C:\Users\echan\Desktop\SIT323\sit323-2024-t1-prac10p\google-cloud-sdk\bin;%PATH%;
+cd C:\Users\echan\Desktop\SIT323\sit323-2024-t1-prac10p
+ECHO Welcome to the Google Cloud CLI! Run "gcloud -h" to get the list of available commands.
+ECHO ---
+ECHO ON
